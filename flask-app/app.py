bd217906194b337f9746db6e94b712436fb29f4e
Cyberphish <113986409+Cyberphish@users.noreply.github.com>
2023-03-15 15:12:33 +0300

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -27,7 +27,12 @@ def pred():
         body = request_data['body']
         features = "\n".join([subject, body])
         prediction = model.predict([features])
-        #         Vocab_list = vectorizer.transform([features]).toarray()
+        vocabulary  = vectorizer.transform([features]).toarray()   
+        bag_of_words = pd.DataFrame(
+               vocabulary, columns=vectorizer.get_feature_names_out())
+        for vector in bag_of_words:
+           if (bag_of_words[vector].values > 0):
+               Vocab_list[bag_of_words[vector].name] = bag_of_words[vector].values[0]
         prediction = f'{prediction}'
         response = f'{Vocab_list}'        
         return jsonify({'prediction': prediction,
@@ -39,7 +44,12 @@ def pred():
         subject = 'funds to share'
         body = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
         features = "\n".join([subject, body])
-        #Vocab_list = vectorizer.transform([features]).toarray()
+        vocabulary  = vectorizer.transform([features]).toarray()   
+        bag_of_words = pd.DataFrame(
+               vocabulary, columns=vectorizer.get_feature_names_out())
+        for vector in bag_of_words:
+           if (bag_of_words[vector].values > 0):
+               Vocab_list[bag_of_words[vector].name] = bag_of_words[vector].values[0]
         prediction = model.predict([features])
         prediction = f'{prediction}'
         response = f'{Vocab_list}'        
