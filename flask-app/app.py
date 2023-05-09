b340962fd5c5a87362214cea28a4e090dbc4a853
Cyberphish <113986409+Cyberphish@users.noreply.github.com>
2023-05-09 16:47:20 +0300

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -20,7 +20,6 @@ def pred():
     global response
     # checking the request type we get from the app
     if (request.method == 'POST'):
-        print('inn')
         subject = ''
         body=''
         Vocab_list = {}
@@ -43,25 +42,3 @@ def pred():
         return jsonify({'prediction': prediction[1],
                         'vocabulary': response
                         }) 
-
-    else:
-        # sending data back to your frontend app
-        subject = 'funds to share'
-        body = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
-        Vocab_list = {}
-        features = "\n".join([subject, body]) 
-        prediction = model.predict([features])
-        encode  = vectorizer.transform([features]).toarray()  
-        bag_of_words = pd.DataFrame(
-                 encode, columns=vectorizer.get_feature_names_out())
-        Vocab_list = {}
-        for vector in bag_of_words:
-           if (bag_of_words[vector].values > 0):
-               Vocab_list[bag_of_words[vector].name] = bag_of_words[vector].values[0]
-        prediction = f'{prediction}'
-        response = f'{Vocab_list}'        
-        return jsonify({'prediction': prediction[1],
-                        'vocabulary': response
-                        }) 
-        
-    
